007885badfd7170da8b17213638b5583020f231b
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-20 19:43:51 +0700

    Section 01 - Chapter 06 ~ Linking Between Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+      Home Page
+    </div>
+  )
 }
