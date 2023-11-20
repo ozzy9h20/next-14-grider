404769d9b5b61f6387ee6e41145eb35081c2c9dc
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-20 20:38:24 +0700

    Section 01 - Chapter 11 ~ Add Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Link from 'next/link'
+import Image from 'next/image'
+import homeImg from 'public/images/home.jpg'
 
 export default function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="Car Factory"
+          fill
+          style={{
+            objectFit: 'cover',
+          }}
+        />
+      </div>
+    </div>
+  )
 }
