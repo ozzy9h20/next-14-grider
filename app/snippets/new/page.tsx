62d2e0bf884b3a23d94fc7ddfe04572c5c01e6d7
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-20 22:21:35 +0700

    Section 02 - Chapter 21 ~ Add Create Snippet Form

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,3 +1,41 @@
 export default function SnippetCreatePage() {
-  return <div>Create Snippet Page</div>
+  return (
+    <form>
+      <h3 className="text-3xl text-center font-bold my-5">Create A Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label
+            className="w-12"
+            htmlFor="title"
+          >
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label
+            className="w-12"
+            htmlFor="code"
+          >
+            Code
+          </label>
+          <textarea
+            name="title"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+        <button
+          type="submit"
+          className="border rounded p-2 bg-blue-200"
+        >
+          Create
+        </button>
+      </div>
+    </form>
+  )
 }
