009a3b3f179cfb0db52260f8ec8a2111f17d9ae6
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-20 22:33:50 +0700

    Section 03 - Chapter 22 ~ Introduce Server Action

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,6 +1,32 @@
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
+
 export default function SnippetCreatePage() {
+  /* ========================================= Functions ======================================== */
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action
+    'use server'
+
+    // Check the user's input and make sure they're valid
+    const title = formData.get('title') as string
+    const code = formData.get('code') as string
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    })
+    console.log('🚀 ~ file: page.tsx:21 ~ createSnippet ~ snippet:', snippet)
+
+    // Redirect the user back to the root route
+    redirect('/')
+  }
+
+  /* ========================================== Output ========================================== */
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="text-3xl text-center font-bold my-5">Create A Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -24,7 +50,7 @@ export default function SnippetCreatePage() {
             Code
           </label>
           <textarea
-            name="title"
+            name="code"
             className="border rounded p-2 w-full"
             id="code"
           />
