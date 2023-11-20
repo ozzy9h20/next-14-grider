201c34bccf4725fa2df3208adda5a56f909e4672
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-20 20:48:07 +0700

    fix: route to home page

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div>
-      <Link href="/home">Home</Link>
+      <Link href="/">Home</Link>
       <Link href="/performance">Performance</Link>
       <Link href="/reliability">Reliability</Link>
       <Link href="/scale">Scale</Link>
