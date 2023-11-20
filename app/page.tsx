d9325c6a9f4a2b708c816bb8694be62360a918f8
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-20 19:51:17 +0700

    Section 01 - Chapter 07 ~ Common UI with Layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import Link from 'next/link'
 
 export default function HomePage() {
-  return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-      Home Page
-    </div>
-  )
+  return <div>Home Page</div>
 }
