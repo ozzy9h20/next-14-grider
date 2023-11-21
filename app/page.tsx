356b48bb77ab17034ff6902b05df160c6e7e8a6e
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-21 07:05:21 +0700

    Section 03 - Chapter 22 ~ Fetching Data with Server Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Home Page</div>
+import { db } from '@/db'
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  return (
+    <div>
+      Home Page
+      {snippets.map((snippet) => (
+        <div>
+          {snippet.id} {snippet.title}
+        </div>
+      ))}
+    </div>
+  )
 }
