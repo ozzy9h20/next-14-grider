821a3765aaaa11a3ff58b4a3c22671cb90e556f2
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-21 20:55:14 +0700

    Section 03 - Chapter 35 ~ Handle Editor Change

--- components/snippet-edit-form.tsx
+++ components/snippet-edit-form.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Editor } from '@monaco-editor/react'
 import type { Snippet } from '@prisma/client'
 
@@ -8,13 +8,20 @@ interface SnippetEditFormProps {
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code)
+
+  const handleEditorChange = (value: string = '') => {
+    setCode(value)
+  }
+
   return (
     <Editor
       height="40vh"
       theme="vs-dark"
       language="javascript"
-      defaultValue={snippet.code}
+      defaultValue={code}
       options={{ minimap: { enabled: false } }}
+      onChange={handleEditorChange}
     />
   )
 }
