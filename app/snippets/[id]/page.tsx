3f60f4b75f0b8a851c97d91487ae6278fa86c698
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-21 18:49:12 +0700

    Section 03 - Chapter 31 ~ Style The Show Snippet Page

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -16,5 +16,18 @@ export default async function SnippetDetailPage(props: SnippetDetailPageProps) {
     return notFound()
   }
 
-  return <div>{snippet.title}</div>
+  return (
+    <div>
+      <div className="flex my-4 justify-between items-center">
+        <h1 className="text-3xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-2">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-400">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  )
 }
