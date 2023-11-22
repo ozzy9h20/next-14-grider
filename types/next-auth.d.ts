6d50ba1c38079225d3c6f70160296c4cccee5989
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-22 16:44:05 +0700

    Section 07 - Chapter 58 ~ Next-Auth Setup

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+// see: https://next-auth.js.org/getting-started/typescript
+
+import NextAuth, { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string
+    } & DefaultSession['user']
+  }
+}
