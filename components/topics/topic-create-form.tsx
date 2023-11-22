5e6db2e6fab4384a0a8197faa3996fa89f3603c3
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-23 03:38:59 +0700

    Section 07 - Chapter 75 ~ Receive Form Data

--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -19,11 +19,13 @@ export default function TopicCreateForm() {
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
+              name="name"
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
             />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
