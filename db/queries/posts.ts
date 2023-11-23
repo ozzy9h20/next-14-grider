2924f40167ccdb88f74b7e634a06464fa9f4e48b
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-24 03:24:31 +0700

    Section 10 - Chapter 108 ~ Top Posts on the HomePage

--- db/queries/posts.ts
+++ db/queries/posts.ts
@@ -28,3 +28,32 @@ export function fetchPostByTopicSlug(slug: string) {
     },
   })
 }
+
+export function fetchTopPost(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: {
+      comments: {
+        _count: 'desc',
+      },
+    },
+    include: {
+      topic: {
+        select: {
+          slug: true,
+        },
+      },
+      user: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
+    },
+    take: 5,
+  })
+}
