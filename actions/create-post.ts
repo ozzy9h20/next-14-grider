413ea1538f597cc093610b25eb0e5f4906aa9fcc
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-23 16:22:07 +0700

    Section 08 - Chapter 86 ~ Apply Validation to Post Creation

--- actions/create-post.ts
+++ actions/create-post.ts
@@ -1,3 +1,35 @@
 'use server'
+import { z } from 'zod'
 
-export async function createPost() {}
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+})
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[]
+    content?: string[]
+    _form?: string[]
+  }
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData,
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  })
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
+  }
+
+  return {
+    errors: {},
+  }
+}
--- components/posts/post-create-form.tsx
+++ components/posts/post-create-form.tsx
@@ -5,17 +5,22 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Textarea,
 } from '@nextui-org/react'
 import FormButton from '@/components/common/form-button'
+import { useFormState } from 'react-dom'
+import { createPost } from '@/actions'
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(createPost, { errors: {} })
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Post</h3>
 
@@ -24,12 +29,16 @@ export default function PostCreateForm() {
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
             />
-            <Input
+            <Textarea
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
             />
 
             <FormButton>Create Post</FormButton>
